6b3a02f47b11b8c126f6bc96de1c085d48ffc7fd
emmett1 <emmett1.2miligrams@gmail.com>
2019-11-10 17:50:36 +0800

    update install instruction and etc

--- rootfs/root/custom_script.sh
+++ rootfs/root/custom_script.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+USER=lfs
+PASSWORD=lfs
+
+useradd -m -G users,wheel,audio,video -s /bin/bash $USER
+passwd -d $USER &>/dev/null
 passwd -d root &>/dev/null
 
 echo "root:root" | chpasswd -c SHA512
+echo "$USER:$PASSWORD" | chpasswd -c SHA512
+
+echo "lfs-livecd" > /etc/hostname
